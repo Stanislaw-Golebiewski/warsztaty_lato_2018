9a63baab92bfde569c6cb98c67e18a488911d547
Stanislaw-Golebiewski <slowik66@o2.pl>
2018-06-05 23:22:45 +0200

    add comments to unused controller and model

--- MEME_API/models/Comment-model.js
+++ MEME_API/models/Comment-model.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
-
-const CommentSchema = (mongoose.Schema = {
-	date: { type: Date, default: Date.now },
-	text: { type: String, required: true },
-	user: { type: String, required: true },
-});
-
-module.exports = mongoose.model("Comment", CommentSchema);
+// const mongoose = require("mongoose");
+//
+// const CommentSchema = (mongoose.Schema = {
+// 	date: { type: Date, default: Date.now },
+// 	text: { type: String, required: true },
+// 	user: { type: String, required: true },
+// });
+//
+// module.exports = mongoose.model("Comment", CommentSchema);
